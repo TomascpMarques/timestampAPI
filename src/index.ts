ea8e19d6adf6268373c4e5320e25805cd559bafc
TomascpMarques <48774061+TomascpMarques@users.noreply.github.com>
2021-06-23 21:42:03 +0100

    timestamp microservice api fCC create git repo

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// Global .env Vars
+require('dotenv').config()
+
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+
+// setup the express app
+const app = express()
+
+app.use(cors({ optionsSuccessStatus: 200 }))
+app.use('/', express.static('public'))
+
+// Start server listening on port XXXX
+app.listen(process.env.PORT, () => {
+   console.log('Server is listening in port', process.env.PORT)
+})
