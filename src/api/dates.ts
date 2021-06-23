24ae44c73a6e9db9bbd58074382dc2e0d67d18af
TomascpMarques <48774061+TomascpMarques@users.noreply.github.com>
2021-06-24 00:56:47 +0100

    Commenting

--- src/api/dates.ts
+++ src/api/dates.ts
@@ -1,7 +1,8 @@
 import express from 'express'
-export const api = express.Router()
+export const apiDates = express.Router()
 
-api.use((req: express.Request, _res: express.Response, next: any) => {
+// Request logger for the api
+apiDates.use((req: express.Request, _res: express.Response, next: any) => {
    console.log(
       `${req.method} ON ${req.url} BY ${
          req.ip
@@ -10,6 +11,14 @@ api.use((req: express.Request, _res: express.Response, next: any) => {
    next()
 })
 
+/**
+ * This is a middleware function that validates the recieved date format.
+ * Validates for the UTC format by a Date().toUTCString(), and the unix time format by a regexp
+ * @param req The recieved Http Request
+ * @param res The response writer
+ * @param next Then next function in the call list (stack?)
+ * @returns If the passed __date format is not valid__, returns with __status code__ `400`(bad user request), and the object `{error: "Invalid Date"}`
+ */
 const reqDateValidator = (
    req: express.Request,
    res: express.Response,
@@ -26,7 +35,10 @@ const reqDateValidator = (
    }
 }
 
-api.route('/:date').get(reqDateValidator, (req, res) => {
+// The complete route is `[app_url]/api/:date`
+// Checks the date "time format" then creates the responsse object acordingly
+// with the unix and utc respective time values
+apiDates.route('/:date').get(reqDateValidator, (req, res) => {
    if (req.params.date.match(/^[0-9]{1,20}$/gm)) {
       res.status(200).json({
          unix: Number(new Date(Number(req.params.date))),
@@ -40,14 +52,17 @@ api.route('/:date').get(reqDateValidator, (req, res) => {
    }
 })
 
-api.route('/').get((_req, res) => {
+// The complete route is `[app_url]/api(/)`
+// this route returns the current time in both formats, unix and utc
+apiDates.route('/').get((_req, res) => {
    res.status(200).json({
       unix: new Date().getTime(),
       utc: new Date().toUTCString(),
    })
 })
 
-api.use('*', (req, res) => {
+// the 404 route for the API
+apiDates.use('*', (req, res) => {
    console.log(req.url, '404')
    res.end('404')
 })
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@ require('dotenv').config()
 
 import express from 'express'
 import cors from 'cors'
-import { api } from './api/dates'
+// Import the api Router
+import { apiDates } from './api/dates'
 
 // setup the express app
 const app = express()
 
+// Define cors politics
 app.use(cors({ optionsSuccessStatus: 200 }))
+
+// Content for the Apps fron-page (instructions)
 app.use('/', express.static('public'))
-app.use('/api', api)
+// Define the router that will handle the "/api" route
+app.use('/api', apiDates)
 
 // Start server listening on port XXXX
 app.listen(process.env.PORT, () => {
